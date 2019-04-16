9f9e6b507424dfb194c2e3d86b82a3c67cb8f5dc
krishna jallepalli <krishna.jallepalli@hotmail.com>
2019-04-16 21:18:02 +0530

    delete krishna java script file

--- krishna.js
+++
@@ -1,8 +0,0 @@
-function getWeather() {
-    weather.getWeather()
-    .then(results => ui.paint(results))
-    .catch(err => {
-        ui.clear(document.getElementById('city').value);
-        console.log(err);
-    });
-}
